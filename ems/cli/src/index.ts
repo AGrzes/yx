f84e9e6ed6403c065f8960c56e1ef610a10fdc1a
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-11-24 21:37:56 +0100

    Lifecycle init

--- ems/cli/src/index.ts
+++ ems/cli/src/index.ts
@@ -5,5 +5,18 @@ import cors from 'cors'
 import scan from './scan'
 import container from './container'
 import { register } from 'ts-node'
-register()
-container()
+
+export interface Lifecycle {
+  init(): Promise<void>
+}
+
+async function main() {
+  register()
+  const c = await container()
+  const lifecycle = c.getAll<Lifecycle>('lifecycle')
+  for (const l of lifecycle) {
+    await l.init()
+  }
+}
+
+main().catch(console.error)
