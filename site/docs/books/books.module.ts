58f5ba1acff31d57dbf32a71173a4897d3007dc7
Grzegorz Abramczyk <abramczykg@gmail.com>
2021-11-24 21:38:26 +0100

    Watchig files and loading EMS

--- site/docs/books/books.module.ts
+++ site/docs/books/books.module.ts
@@ -1,7 +1,29 @@
-import { ContainerModule } from 'inversify'
+import { ContainerModule, inject, injectable } from 'inversify'
+import { Store } from '@agrzes/yx-ems-api'
 
-const serverModule = new ContainerModule(() => {
-  console.log('Server module loaded from docs')
+import 'reflect-metadata'
+import * as chokidar from 'chokidar'
+import fm from 'front-matter'
+import { readFile } from 'fs/promises'
+import { join } from 'path'
+
+@injectable()
+class BooksLoader {
+  constructor(@inject('ems.Store') private store: Store) {}
+  async init() {
+    const watch = chokidar.watch(join(__dirname, '**/*.mdx'))
+    const consume = async (p: string) => {
+      this.store.store(p, fm(await readFile(p, 'utf-8')).attributes)
+    }
+    watch.on('add', consume)
+    watch.on('change', consume)
+  }
+}
+
+const serverModule = new ContainerModule((bind) => {
+  bind(BooksLoader).toSelf().inSingletonScope()
+
+  bind('lifecycle').toService(BooksLoader)
 })
 
 export default serverModule
